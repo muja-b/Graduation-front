550316cc85c493af976188fdf881e50353e0debb
muja-b <s11819212@stu.najah.edu>
2022-12-27 21:13:36 +0200

    added deafult keyboard

--- signUp_signIn/login/src/App.js
+++ signUp_signIn/login/src/App.js
@@ -1,15 +1,87 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import React from "react"
-import { BrowserRouter , Route ,Routes} from 'react-router-dom';
-import Auth from "./Auth";
+import { useState } from 'react';
+import { ThemeProvider } from "styled-components";
+import DefaultKeyboard from './Components/features/DefaultKeyboard'
+import FooterMenu from './Components/features/FooterMenu'
+import { defaultTheme, themesOptions } from "./style/theme";
+import { GlobalStyles } from "./style/global.js";
+import useLocalPersistState from "./hooks/useLocalPersistState";
+import {
+  SOUND_MODE,
+  soundOptions,
+  DEFAULT_SOUND_TYPE,
+  DEFAULT_SOUND_TYPE_KEY,
+} from "./Components/features/sound/sound";
+import React from 'react';
+
 function App() {
+  const [theme, setTheme] = useState(() => {
+    const stickyTheme = window.localStorage.getItem("theme");
+    if (stickyTheme !== null) {
+      const localTheme = JSON.parse(stickyTheme);
+      const upstreamTheme = themesOptions.find(
+        (e) => e.label === localTheme.label
+      ).value;
+      // we will do a deep equal here. In case we want to support customized local theme.
+      const isDeepEqual = localTheme === upstreamTheme;
+      return isDeepEqual ? localTheme : upstreamTheme;
+    }
+    return defaultTheme;
+  });
+
+  // local persist game mode setting
+  const [soundMode, setSoundMode] = useLocalPersistState(true, SOUND_MODE);
+
+  const [soundType, setSoundType] = useLocalPersistState(
+    DEFAULT_SOUND_TYPE,
+    DEFAULT_SOUND_TYPE_KEY
+  );
+  const handleThemeChange = (e) => {
+    window.localStorage.setItem("theme", JSON.stringify(e.value));
+    setTheme(e.value);
+  };
+   const toggleSoundMode = () => {
+    setSoundMode(!soundMode);
+  };
+
+  const toggleMusicMode = () => {
+    setIsMusicMode(!isMusicMode);
+  };
+    const handleSoundTypeChange = (e) => {
+    setSoundType(e.label);
+  };
+   const toggleFocusedMode = () => {
+    setIsFocusedMode(!isFocusedMode);
+  };
+    const [isFocusedMode, setIsFocusedMode] = useState(
+    localStorage.getItem("focused-mode") === "true"
+  );
+  const [isMusicMode, setIsMusicMode] = useState(false);
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/auth" element={<Auth />} />
-    </Routes>
-  </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <div className='canvas'>
+        <GlobalStyles>
+        </GlobalStyles>
+          <div className="App">
+          <DefaultKeyboard letters={["ص","ض"]} soundMode={soundMode}
+              soundType={soundType}>
+          </DefaultKeyboard> 
+      </div> 
+      </div>
+      <FooterMenu
+            themesOptions={themesOptions}
+            theme={theme}
+            soundMode={soundMode}
+            toggleSoundMode={toggleSoundMode}
+            soundOptions={soundOptions}
+            soundType={soundType}
+            handleSoundTypeChange={handleSoundTypeChange}
+            handleThemeChange={handleThemeChange}
+            toggleFocusedMode={toggleFocusedMode}
+            toggleMusicMode={toggleMusicMode}
+            isFocusedMode={isFocusedMode}
+          ></FooterMenu>
+    </ThemeProvider>
+    
   );
 }
 
